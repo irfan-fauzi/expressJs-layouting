ad5d73e04b4cc4e1aad29f232bb278cfd77fd1b8
irfan-fauzi <irfanwintech@yahoo.com>
2021-08-25 14:47:42 +0700

    clear

--- app.js
+++ app.js
@@ -4,17 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  //res.send('Hello World!')
-  // res.json([
-  //   {
-  //     name: "irfan",
-  //     email: "asdas"
-  //   },
-  //   {
-  //     name: "irfan",
-  //     email: "asdas"
-  //   }
-  // ])
+  
   res.sendFile('./view/index.html', {
     root: __dirname
   })
